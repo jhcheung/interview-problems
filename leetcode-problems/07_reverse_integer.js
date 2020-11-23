00b311d5155311f9d1949e8de3bfad8191028204
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2020-11-22 20:54:50 -0500

    reverseInPlace solution added optimizing storage

--- leetcode-problems/07_reverse_integer.js
+++ leetcode-problems/07_reverse_integer.js
@@ -1,18 +1,42 @@
 // Given a 32-bit signed integer, reverse digits of an integer.
 // https://leetcode.com/problems/reverse-integer/
 // O(log n) runtime, scales with number of digits in an integer, which is log(n)
-// O(log n) storage, scales with number of digits in an integer again.
+// O(log n) storage for reverse(), scales with number of digits in an integer again.
+// O(1) storage for reverseInPlace(), stays in num/result.
 
 /**
  * @param {number} x
  * @return {number}
  */
+const reverseInPlace = function(x) {
+    const sign = x > 0 ? 1 : -1;
+    if (sign < 0) {
+        x = x * -1
+    }
+
+    let num = 0;
+    let place = 1;
+    let magnitude = Math.floor(Math.log10(x))
+
+    while (magnitude >= 0) {
+        let digit = Math.floor(x / (10 ** magnitude));
+        x = x % (10 ** magnitude);
+        num += digit * place;
+        place = place * 10;
+        magnitude -= 1;
+        console.log(x, digit, num);
+    }
+    console.log("=====");
+
+    if (num >= 2 ** 31 * 1 || num <= 2 ** 31 * -1) {
+        return 0;
+    }
+
+    return num * sign;
+};
+
 const reverse = function(x) {
     const arrayOfNums = []
-
-    // if (x >= 2 ** 31 * 1 || x <= 2 ** 31 * -1) {
-    //     return 0;
-    // }
 
     const sign = x > 0 ? 1 : -1;
     if (sign < 0) {
@@ -43,9 +67,9 @@ const reverse = function(x) {
     return num * sign;
 };
 
-console.assert(reverse(123) === 321);
-console.assert(reverse(-123) === -321);
-console.assert(reverse(120) === 21);
-console.assert(reverse(0) === 0);
-console.assert(reverse(2 ** 31 * -1) === 0);
-console.assert(reverse(1534236469) === 0)
+console.assert(reverseInPlace(123) === 321);
+console.assert(reverseInPlace(-123) === -321);
+console.assert(reverseInPlace(120) === 21);
+console.assert(reverseInPlace(0) === 0);
+console.assert(reverseInPlace(2 ** 31 * -1) === 0);
+console.assert(reverseInPlace(1534236469) === 0);
